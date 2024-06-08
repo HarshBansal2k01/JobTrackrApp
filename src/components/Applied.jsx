16ce52e517a2c5f4be9416443fb1581105baf46d
HarshBansal2k01 <ihbbro14@gmail.com>
2024-06-08 20:46:51 +0530

    completed the frontend logic of the card

--- src/components/Applied.jsx
+++ src/components/Applied.jsx
@@ -5,12 +5,18 @@ import InputLabel from "@mui/material/InputLabel";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import TextField from "@mui/material/TextField";
 import InsertLinkIcon from "@mui/icons-material/InsertLink";
-import Card from "react-bootstrap/Card";
+import { Card, Form } from "react-bootstrap";
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
 import { Button } from "@mui/material";
 import MenuItem from "@mui/material/MenuItem";
 import InputAdornment from "@mui/material/InputAdornment";
+import BusinessIcon from "@mui/icons-material/Business";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import AttachMoneyOutlinedIcon from "@mui/icons-material/AttachMoneyOutlined";
+import FormHelperText from "@mui/material/FormHelperText";
+import Select from "@mui/material/Select";
+import { Link } from "react-router-dom";
 
 const StatusDrop = [
   {
@@ -76,43 +82,120 @@ function Applied() {
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
+                flexDirection: "column",
               }}
             >
-              <Card style={{ width: "25rem" }}>
-                <Card.Body>
-                  {formData.map(
-                    (
-                      jobData // Use entire object 'jobData'
-                    ) => (
-                      <div key={jobData.companyName}>
-                        {" "}
-                        <Card.Title>
-                          Company Name: {jobData.companyName}
-                        </Card.Title>
-                        <Card.Title>
-                          Role Applied For: {jobData.role}
-                        </Card.Title>
-                        <Card.Title>
-                          Salary Range: {jobData.salaryRange}
-                        </Card.Title>
-                        <Card.Title>
-                          Status: {jobData.status}
-                        </Card.Title>
-                        <Card.Title>
-                          Link: {jobData.link}
-                        </Card.Title>
-                       
+              {formData.map((jobData) => (
+                <Card
+                  key={jobData.companyName}
+                  style={{ width: "25rem", marginBottom: "1rem" }}
+                >
+                  <Card.Body>
+                    <Card.Title
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        fontSize: "24px",
+                        fontWeight: "bold",
+                        fontFamily: "Arial, sans-serif",
+                        textAlign: "left",
+                      }}
+                    >
+                      <BusinessIcon style={{ fontSize: 30, marginRight: 10 }} />{" "}
+                      {jobData.companyName}
+                    </Card.Title>
+
+                    <Card.Title
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        fontSize: "20px",
+                        fontWeight: "normal",
+                        color: "#555",
+                        fontFamily: "Arial, sans-serif",
+                        textAlign: "left",
+                      }}
+                    >
+                      <AccountCircleOutlinedIcon
+                        style={{ fontSize: 30, marginRight: 10 }}
+                      />
+                      {jobData.role}
+                    </Card.Title>
+
+                    <Card.Title
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        fontSize: "18px",
+                        fontWeight: "normal",
+                        color: "#777",
+                        fontFamily: "Arial, sans-serif",
+                        textAlign: "left",
+                      }}
+                    >
+                      <AttachMoneyOutlinedIcon
+                        style={{ fontSize: 30, marginRight: 10 }}
+                      />
+                      {jobData.salaryRange}
+                    </Card.Title>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <div style={{ flex: 1 }}>
+                        <div style={{ textAlign: "left" }}>
+                          <Button
+                            variant="contained"
+                            style={{
+                              backgroundColor: "white",
+                              color: "black",
+                              height: "50px",
+                              width: "80%",
+                              justifyContent: "flex-start",
+                              marginBottom:"19px"
+                            }}
+                          >
+                            <Link
+                              to={jobData.link}
+                              style={{ alignItems: "left" }}
+                            >
+                              <InputAdornment position="start">
+                                <InsertLinkIcon />
+                              </InputAdornment>
+                              
+                            </Link>
+                            Company
+                          </Button>
+                        </div>
                       </div>
-                    )
-                  )}
-                </Card.Body>
-              </Card>
+                      <div style={{ flex: 1 }}>
+                        <FormControl sx={{ m: 1 }}>
+                          <InputLabel id="demo-simple-select-helper-label">
+                            Status
+                          </InputLabel>
+                          <Select
+                            labelId="demo-simple-select-helper-label"
+                            id="demo-simple-select-helper"
+                            value={jobData.status}
+                            label="Status"
+                            style={{ width: "100%" }} // Make the select take full width
+                          >
+                            {StatusDrop.map((status, index) => (
+                              <MenuItem key={index} value={status.value}>
+                                {status.value}
+                              </MenuItem>
+                            ))}
+                          </Select>
+                          <FormHelperText>Change Status</FormHelperText>
+                        </FormControl>
+                      </div>
+                    </div>
+                  </Card.Body>
+                </Card>
+              ))}
             </div>
           ) : (
             <div>No data available</div>
           )}
         </div>
-        <div style={{ marginTop: "10px" }}>
+        <div style={{ marginTop: "10px" , marginBottom:"10px" }}>
           <Button variant="contained" onClick={() => setIsOpen(true)}>
             {" "}
             Add Job{" "}
